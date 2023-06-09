c9a5a18e7f20dcc617c670e0b740134cb8fe62b6
Adryelgcouto <adryelgcouto@gmail.com>
2023-06-09 14:28:52 -0300

    layout 90% finalizado

--- src/subscriptions/subscriptions.jsx
+++ src/subscriptions/subscriptions.jsx
@@ -1,22 +1,43 @@
 import Prata from "../assets/logo-plano-branco.png";
-import { ContentSubscriptions } from "./subscriptions.styled";
-import Seta from '../assets/rota-subscriptions-fa-solid_arrow-left.svg'
+import { ContentSubscriptions, InputValidate } from "./subscriptions.styled";
+import Seta from "../assets/rota-subscriptions-fa-solid_arrow-left.svg";
+import check from "../assets/rota-subscriptions-Vector.svg";
+import dolar from '../assets/rota-subscriptions-fa-solid_money-bill-wave.svg'
 export function Subscriptions() {
   return (
     <ContentSubscriptions>
-    <header>
-    <img src={Seta} alt="" />
+      <header>
+        <img src={Seta} alt="" />
       </header>
       <main>
         <div>
           <img src={Prata} alt="" />
           <h3>Driven Plus</h3>
-          <div>
-            
-          </div>
         </div>
+        <div>
+          <div>
+            <img src={check} alt="" />
+            <span>Benefícios:</span>
+          </div>
+          <p>1. Brindes exclusivos</p>
+          <p>2. Materiais bônus de web </p>
+          <div>
+            <img src={dolar} alt="" />
+            <span>Preco:</span>
+          </div>
+          <p>R$ 39,99 cobrados mensalmente</p>
+        </div>
+        <InputValidate>
+        <input type="text" placeholder="Nome impresso no cartão" />
+        <input type="text" placeholder="Digitos do cartão" />
+        <div>
+          <input type="text" placeholder="Código de segurança" />
+          <input type="text" placeholder="validade" />
+        </div>
+        <button>ASSINAR</button>
+        </InputValidate>
+        
       </main>
     </ContentSubscriptions>
-  
   );
 }
--- src/subscriptions/subscriptions.styled.jsx
+++ src/subscriptions/subscriptions.styled.jsx
@@ -1,13 +1,85 @@
 import styled from "styled-components";
 
-
 export const ContentSubscriptions = styled.div`
+  main {
+    p{
+      color: white;
+    }
+    & div:nth-child(1) {
+      h3 {
+        margin-left: 20px;
+        margin-top: 12px;
+        margin-bottom: 22px;
+      }
+      img {
+        width: 139.38px;
+      }
+      width: 100vw;
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+    }
+    & div:nth-child(2) {
+      margin-left: 40px;
+      span {
+        color: white;
+        font-size: 16px;
+      }
+      & div:nth-child(1) {
+
+        display: flex;
+        flex-direction: row;
+        margin-bottom: 10px;
+        img {
+          width: 20px;
+          height: 20px;
+          margin-right: 1px;
+        }
+      }
+      p{
+        margin-left: 2px;
+      }
+      & div:nth-child(4) {
+        margin-top: 12px;
+      }
+    }
+  }
+  h3 {
+    color: white;
+    font-size: 32px;
+    font-weight: bold;
+  }
+`;
+
+export const InputValidate = styled.div`
+margin-top: 34px;
+margin-left: 40px;
   display: flex;
-  align-items: center;
-  justify-content: center;
   flex-direction: column;
-  h3{
-    color: #FFFFFF;
+  input{
+    margin-bottom: 8px;
+    width: 299px;
+    height: 52px;
+    border-radius: 8px;
+    border: none;
+    padding-left:14px ;
+  }
+  div{
+    input{
+      width: 145px;
+      margin-right: 9px;
+    }
+
+  }
+  button{
+    width: 299px;
+    height: 54px;
+    border-radius: 8px;
+    border: none;
+    background-color: #FF4791;
+    color: white;
+    font-weight: bold;
+    font-size: 14px;
   }
 
 `
